70064054bee5daac428376cc3c16510c9fb02b50
Irame <irame94@gmail.com>
2017-03-22 01:49:16 +0100

    Added a simple animator funktion 'WalkThroughBreadthFirstSearch'

---
+++ FHWS-TI-Solution/Graphs/Sheet01/Animations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Graphs
+{
+    partial class Graph<TVertex>
+    {
+        public bool IsAnimationPlaying { get; private set; } = false;
+
+        public void WalkThroughBreadthFirstSearch(int speed = 500)
+        {
+            if (IsAnimationPlaying) return;
+
+            IsAnimationPlaying = true;
+            Task.Run(() =>
+            {
+                BreadthFirstSearch(Vertices.First(), vertex =>
+                {
+                    Task.Delay(speed).Wait();
+                    vertex.BackgroundBrush = Brushes.LimeGreen;
+                });
+            }).ContinueWith(task => IsAnimationPlaying = false);
+        }
+    }
+}
